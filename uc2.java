d3923ec52c83eaf65c83c8cc781132439ff78b70
adityaveeras3837 <as3837@srmist.edu.in>
2026-02-17 10:53:13 +0530

    uc2 completed

---
+++ uc2.java
@@ -0,0 +1,12 @@
+public class OOPSBanner {
+    public static void main(String[] args) {
+
+        // O
+        System.out.println(" *****    *****   *****   ***** ");
+        System.out.println("*     *  *     *  *    *  *     ");
+        System.out.println("*     *  *     *  *****   ***** ");
+        System.out.println("*     *  *     *  *            *");
+        System.out.println(" *****    *****   *       ***** ");
+
+    }
+}
